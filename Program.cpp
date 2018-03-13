4cafacd09d4700b1d99f3ec6a62296b1f3f0d6a0
Bartosz Rodziewicz <bartoszka1996@gmail.com>
2018-03-13 11:04:59 +0100

    Java-ify it.

---
+++ Program.cpp
@@ -0,0 +1,12 @@
+//
+// Created by barto on 13.03.18.
+//
+
+#include <iostream>
+#include "Program.h"
+
+using namespace std;
+
+void Program::start() {
+	cout<<"To be implemented!"<<endl;
+}
---
+++ Program.h
@@ -0,0 +1,15 @@
+//
+// Created by barto on 13.03.18.
+//
+
+#ifndef DININGPHILOSOPHERSPROBLEM_PROGRAM_H
+#define DININGPHILOSOPHERSPROBLEM_PROGRAM_H
+
+
+class Program {
+public:
+	void start();
+};
+
+
+#endif //DININGPHILOSOPHERSPROBLEM_PROGRAM_H
