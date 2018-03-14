3b6dca10d4b3862cc84080b11e6b99b09af8db4d
Bartosz Rodziewicz <bartoszka1996@gmail.com>
2018-03-14 16:16:02 +0100

    Change number of philosophers to correct one

--- Program.h
+++ Program.h
@@ -2,7 +2,7 @@
 // Created by barto on 13.03.18.
 //
 
-#define numberOfPhilosophers 6
+#define numberOfPhilosophers 5
 
 #ifndef DININGPHILOSOPHERSPROBLEM_PROGRAM_H
 #define DININGPHILOSOPHERSPROBLEM_PROGRAM_H
