eb32c313ade00e9070f3ac640b02535811de9b69
Bartosz Rodziewicz <bartoszka1996@gmail.com>
2018-03-14 11:33:42 +0100

    Revert "Trying to make app stop when key is pressed"
    This reverts commit 43651ea1477338dfd98f2e44ad8180256cfdd85e.
    
    ncurses have needed function. gonna implement then.

--- Program.cpp
+++ Program.cpp
@@ -3,21 +3,10 @@
 //
 
 #include <iostream>
-#include <thread>
-#include <conio.h>
 #include "Program.h"
 
 using namespace std;
 
 void Program::start() {
-
-	char c;
-	std::cout<<"press esc to exit! "<<std::endl;
-	while(true)
-	{
-		cout<<"xxx";
-		c=getch();
-		if (c=='q')
-			break;
-	}
+	cout<<"To be implemented!"<<endl;
 }
